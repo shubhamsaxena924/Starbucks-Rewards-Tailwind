70c9a79a56eaa808887eef287eba6bf627620fc0
shubhamsaxena924 <shubham.saxena_cs.ccv19@gla.ac.in>
2021-07-09 10:05:53 +0530

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,46 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     maxWidth: {
-      144: "144rem",
+      144: "90rem",
       81: "81rem",
     },
+    maxHeigh: {
+      81: "50.625rem",
+    },
     minWidth: {
-      81: "81rem",
+      200: "50rem",
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1540px",
+      // => @media (min-width: 1536px) { ... }
+
+      3000: "3000px",
     },
     extend: {
       backgroundImage: (theme) => ({
         star:
           "url('https://www.starbucks.com/weblx/images/rewards/hero/xl-hero-desktop_2021.png')",
+        mobile_star:
+          "url('https://www.starbucks.com/weblx/images/rewards/hero/hero-mobile_2021.jpg')",
       }),
       spacing: {
         576: "144rem",
-        324: "81rem",
+        200: "50rem",
+        25: "6.25rem",
+        mobile: "133.33%",
+        star: "56.25%",
       },
     },
   },
